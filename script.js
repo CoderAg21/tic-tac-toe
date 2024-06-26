0db6d99fcf750b6c243c55f5e3edf679ace5e4df
CoderAg21 <84915319+CoderAg21@users.noreply.github.com>
2024-06-26 22:51:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//global scope
 let grids;
 let input = 'X';
 let winnerName = document.querySelector(".winnerName");
@@ -5,11 +6,19 @@ let restart = document.getElementById("restart");
 let winningCase = [
     [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]
 ]
+let gridArr = [];
+let count = 0;
+let win = false;
 let container = document.querySelector(".container")
-for (let i = 1; i < 10; i++) {
+
+
+
+//This is the engine of  the game..
+for (let i = 1; i < 10; i++) {       //logic to append the grid  in  DOM..
     grids = document.createElement("div");
     grids.classList.add("grids", `grid${i}`);
     container.appendChild(grids);
+    gridArr.push(grids)
 
     // listening the event of each grid
     grids.addEventListener('click', () => {
@@ -33,7 +42,7 @@ for (let i = 1; i < 10; i++) {
                 gridText2.style.background = 'linear-gradient(#6adf6a, yellow)'
                 gridText3.style.background = 'linear-gradient(#6adf6a, yellow)'
                 winnerName.innerText = `'${input}' is Winner..`
-
+                win = true;
                 setTimeout(() => {
                     container.style.display = "none";
                     restart.style.display = 'block';
@@ -41,31 +50,46 @@ for (let i = 1; i < 10; i++) {
                     gridText2.style.background = ''
                     gridText3.style.background = ''
                 }, 1000);
-               
-                    
-              
-            }
 
-        })
-      
-            
-            if (input == "X") {
-                input = 'O';
             }
             else {
-                input = "X";
+                return;
             }
-    
-       
+
+        });
+
+        // logic for  match draw
+        gridArr.forEach((e) => {
+            if (!e.innerText) {
+                count += 1
+            }
+        });
+        if (count == 1 && !win) {
+            winnerName.innerText = `Match Draw..`;
+            container.style.display = "none";
+            restart.style.display = 'block';
+        };
+        count = 1;
+
+
+        //logic to change the symbol
+        if (input == "X") {
+            input = 'O';
+        }
+        else {
+            input = "X";
+        }
+
+
     });
 
-    restart.addEventListener("click",()=>{
+    restart.addEventListener("click", () => {
         container.style.display = "grid";
-                winnerName.innerText = ``
-                restart.style.display = 'none';
-               
-                document.querySelector(`.grid${i}`).innerText = '';
-                input = "X";
+        winnerName.innerText = ``
+        restart.style.display = 'none';
+
+        document.querySelector(`.grid${i}`).innerText = '';
+        input = "X";
 
     })
 };
